544e99e451e33b910c745c689f54a21709e1a251
Geonmo <ry840901@gmail.com>
2015-09-08 01:41:26 +0900

    Add comment.

--- Week2_Variable&Operator/9_float.cpp
+++ Week2_Variable&Operator/9_float.cpp
@@ -4,7 +4,7 @@ int main()
 {
   float a=1.050000f;
   float b= a*100.0f;
-  std::cout<<(int)b<<std::endl; // 당연히 105라고 생각하겠지만,
+  std::cout<<(int)b<<std::endl; // float->int로 강제 변환(소수 버림). 당연히 105라고 생각하겠지만,
 
 
   return 0;
