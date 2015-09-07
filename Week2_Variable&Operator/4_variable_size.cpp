6359758cccc66864033dd088df298fd0c79730d5
Geonmo <ry840901@gmail.com>
2015-09-08 01:15:03 +0900

    Add "long int"== int64_t

--- Week2_Variable&Operator/4_variable_size.cpp
+++ Week2_Variable&Operator/4_variable_size.cpp
@@ -1,11 +1,12 @@
 #include<iostream>
-
+#include<stdint.h>
 int main()
 {
 
   // 선언(Declare) 부분
   char character;
   int integer;
+  int64_t bigInt;
   float floating_single;
   double floating_double;
 
@@ -17,6 +18,7 @@ int main()
   // 정의(Define) 부분
   character = 'a';
   integer = 64;
+  bigInt = 64;
   floating_single = 42.4f;
   floating_double = 42.44322233522341;
 
@@ -26,6 +28,7 @@ int main()
   std::cout<<sizeof(character)<<" "<<sizeof(integer)<<" "<<sizeof(floating_single)<<" "<<sizeof(floating_double)<<std::endl;
   std::cout<<sizeof(intPointer)<<" "<<sizeof(floatPointer)<<std::endl;
 
+  std::cout<<"sizeof(int64_t) : "<<sizeof(bigInt)<<std::endl; 
 
   return 0;     // 정상종료
 
