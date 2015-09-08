96c892798bd8f08528ac15e65b11195cf9548a77
Geonmo <ry840901@gmail.com>
2015-09-08 22:04:21 +0900

    for loop to 10^5.

--- Week2_condition&loop/7_for_loop.cpp
+++ Week2_condition&loop/7_for_loop.cpp
@@ -1,9 +1,9 @@
 #include<iostream>
 
 int main(){
-  int sum =0;
+  long long int sum =0;
   // for( 초기값 ; 조건값 ; Scope가 끝난 후 할 일)
-  for( int i= 1 ; i<=100; i++) {
+  for( int i= 1 ; i<=100000; i++) {
     sum = sum + i;
   }
   std::cout<<sum<<std::endl;
