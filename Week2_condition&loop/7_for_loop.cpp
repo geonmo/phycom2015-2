954921424f2a6f8ea5b13fd5675f284910fc6018
Geonmo <ry840901@gmail.com>
2015-09-08 22:32:31 +0900

    changing algorithm.

--- Week2_condition&loop/7_for_loop.cpp
+++ Week2_condition&loop/7_for_loop.cpp
@@ -1,11 +1,9 @@
 #include<iostream>
 
 int main(){
-  long long int sum =0;
   // for( 초기값 ; 조건값 ; Scope가 끝난 후 할 일)
-  for( int i= 1 ; i<=100000; i++) {
-    sum = sum + i;
+  for( int i= 0 ; i< 100; i++) {
+    std::cout<<"이번에 새로운 i = "<<i<<std::endl;
   }
-  std::cout<<sum<<std::endl;
   return 0;
 }
