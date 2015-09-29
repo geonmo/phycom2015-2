95a399fcdab951b8e385ba323a80152813358e85
Geonmo <ry840901@gmail.com>
2015-09-29 18:46:29 +0900

    Add typeid to identificate class name.

--- Week5_inheritance/example1_employee/Step3_AddTemporaryJob/SalaryManager.cpp
+++ Week5_inheritance/example1_employee/Step3_AddTemporaryJob/SalaryManager.cpp
@@ -3,23 +3,29 @@
 #include"PartTime.h"
 #include"Permanent.h"
 #include"Temporary.h"
+#include<typeinfo>
+#include<cstdio>
 int main(){
 
   // Test suite3. 
   vector<Employee*> em_list;
   Permanent em1("박철수","경리");
-  PartTime em2("한영희",6000);
+  PartTime* em2 = new PartTime("한영희",6000);
   Temporary em3("김민수","영업");
   em_list.push_back(&em1);
-  em_list.push_back(&em2);
+  em_list.push_back(em2);
   em_list.push_back(&em3);
   em_list[0]->doWork();
   em_list[0]->doWork();
   em_list[0]->doWork();
   em_list[0]->goVacation();
   em_list[0]->receiveSalary();
-
-  PartTime* em_part = dynamic_cast<PartTime*>(em_list[1]);
+  PartTime* em_part;
+  // Compare between PartTime class and em_list[1]'s contents.  em_list[1] == Employee,  *em_list[1] == PartTime  
+  if( typeid( PartTime ) == typeid(*em_list[1])) {
+    //std::cout<<"em_list[1] is a PartTime class's instance"<<std::endl;
+    em_part = dynamic_cast<PartTime*>(em_list[1]);
+  }
   em_part->doWork();
   em_part->doWork();
   em_part->doWork();
@@ -32,7 +38,8 @@ int main(){
   em_list[2]->doWork();
   em_list[2]->goVacation();
   em_list[2]->receiveSalary();
-
+  
+  delete em2;
 
   return 0;
 
